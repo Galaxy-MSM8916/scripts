32d80dd15b2a379d0e8b7ef466a72fd2a2e9a469
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-07-15 07:33:12 -0400

    Update artifact script
     - Fix domain/arch separation
    
     - Rename generate_device_list -> optimise_device_list
    
    Change-Id: I4f01d7b8891c7eceeea9b15035d3e9c0ed25af38

--- generate_artifacts-google-1.sh
+++ generate_artifacts-google-1.sh
@@ -17,7 +17,7 @@ function update_job_repo() {
     fi
 }
 
-function generate_device_list() {
+function optimise_device_list() {
     [ -d "$search_path" ] || (>&2 update_job_repo)
     [ -z $devices_full ] && devices_full=`find $search_path -name '*txt' | xargs grep DEVICES`
 
@@ -45,7 +45,9 @@ function find_arch() {
 
 function get_html_home() {
 # first arg - device name
-   arch=`find_arch $1`
+    local arch=`find_arch $1`
+
+    [ -z $arch ] && arch=msm8916
 
     echo /var/www/download.${arch}.com/public_html
 }
@@ -64,30 +66,29 @@ function fix_html_home_perms() {
 
 function link_artifacts() {
 # arg1: find_dir  arg2: out_dir arg3: device
-    local FIND_DIR=$1
-    local OUT_DIR=$2
-    local DEVICE=$3
-    for j in `find $FIND_DIR -mindepth 1 -maxdepth 1 -type f| egrep -iv torrent\|\.part`; do
+    local find_dir=$1
+    local out_dir=$2
+    local device=$3
+    for j in `find $find_dir -mindepth 1 -maxdepth 1 -type f| egrep -iv torrent\|\.part`; do
 	    # ! [ -f $DEST/$(basename $j) ] && 
 	    #echo "Linking $j --> $DEST/$(basename $j) ..."
 	    I_DIR=$(dirname $j)
 
-	    [ -e $OUT_DIR/$(basename $j) ] || ln $j $OUT_DIR/$(basename $j)
-	    if [ $? -ne 0 ] && ! [ -e $OUT_DIR/$(basename $j) ]; then
-		   ln -s $j $OUT_DIR/$(basename $j)
+	    [ -e $out_dir/$(basename $j) ] || ln $j $out_dir/$(basename $j)
+	    if [ $? -ne 0 ] && ! [ -e $out_dir/$(basename $j) ]; then
+		   ln -s $j $out_dir/$(basename $j)
             fi
 
-	    if [ -e "$I_DIR/$DEVICE" ]; then
-		    for k in `find "$I_DIR/$DEVICE" -type f | egrep -iv torrent\|\.part`; do
-			    [ -e "$OUT_DIR/$(basename $k)" ] && ln $k $OUT_DIR
-			    [ $? -ne 0 ] && ln -s $k $OUT_DIR
+	    if [ -e "$I_DIR/$device" ]; then
+		    for k in `find "$I_DIR/$device" -type f | egrep -iv torrent\|\.part`; do
+			    [ -e "$out_dir/$(basename $k)" ] && ln $k $out_dir
+			    [ $? -ne 0 ] && ln -s $k $out_dir
 	            done
             fi
     done
 }
 
 
-HTML_HOME=/var/www/download.msm8916.com/public_html
 TRANSMISSION_OUT=/var/lib/transmission-daemon/downloads/
 T_OUT=/var/lib/transmission-daemon/.config/transmission-daemon/torrents/
 T_BIN=/usr/bin/transmission-remote
@@ -98,7 +99,7 @@ fi
 
 sanitize_html_home
 update_job_repo
-generate_device_list
+optimise_device_list
 
 for i in `find ${T_OUT} -type f -name 'TWRP*'`; do
 #    FILE_NAME=$(basename $i | sed s'/.zip//'g);
@@ -126,7 +127,7 @@ for i in `find ${T_OUT} -type f -name 'TWRP*'`; do
 #    echo
 done
 
-
+# TODO: Generate for all archs
 for i in `find ${T_OUT} -type f -name 'open_gapps*'`; do
 #    FILE_NAME=$(basename $i | sed s'/.zip//'g);
 #    FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
@@ -159,7 +160,7 @@ for i in `find ${T_OUT} -type f -name 'rr*torrent'`; do
     FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
     DEVICE_NAME=$(echo $FILE_NAME | cut -d '-' -f 3)
     BUILD_DATE=$(echo $FILE_NAME | cut -d '_' -f 3)
-    OUT_DIR=`get_html_home $DEVICE`/ResurrectionRemix/$DEVICE_NAME/$BUILD_DATE
+    OUT_DIR=`get_html_home $DEVICE_NAME`/ResurrectionRemix/$DEVICE_NAME/$BUILD_DATE
     mkdir -p $OUT_DIR
 #    F_DIR="$(dirname $i)"
     F_DIR=${TRANSMISSION_OUT}/${FILE_NAME}
@@ -182,7 +183,7 @@ for i in `find ${T_OUT} -type f -name 'lineage-1*torrent'`; do
     FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
     DEVICE_NAME=$(echo $FILE_NAME | cut -d '-' -f 3)
     BUILD_DATE=$(echo $FILE_NAME | cut -d '_' -f 3)
-    OUT_DIR=`get_html_home $DEVICE`/LineageOS/$DEVICE_NAME/$BUILD_DATE
+    OUT_DIR=`get_html_home $DEVICE_NAME`/LineageOS/$DEVICE_NAME/$BUILD_DATE
     mkdir -p $OUT_DIR
 #    F_DIR="$(dirname $i)"
     F_DIR=${TRANSMISSION_OUT}/${FILE_NAME}
@@ -205,7 +206,7 @@ for i in `find ${T_OUT} -type f -name 'lineage-go-1*torrent'`; do
     FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
     DEVICE_NAME=$(echo $FILE_NAME | cut -d '-' -f 4)
     BUILD_DATE=$(echo $FILE_NAME | cut -d '_' -f 3)
-    OUT_DIR=`get_html_home $DEVICE`/LineageOS_Go/$DEVICE_NAME/$BUILD_DATE
+    OUT_DIR=`get_html_home $DEVICE_NAME`/LineageOS_Go/$DEVICE_NAME/$BUILD_DATE
     mkdir -p $OUT_DIR
 #    F_DIR="$(dirname $i)"
     F_DIR=${TRANSMISSION_OUT}/${FILE_NAME}
