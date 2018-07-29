ef8c26ea51246d08d154029e283df1a61345cfc7
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-07-29 17:20:13 -0400

    common/update: Fix rebase arguments
    Change-Id: I565cb822b35c7361dcbe8739ce23241a34781447

--- common/update.sh
+++ common/update.sh
@@ -11,7 +11,7 @@ function update_repo() {
         echo "Found git repo at $git_dir"
         local repo=`dirname $git_dir`
         echo "Updating ${git_dir}..."
-        git -C $repo fetch && git -C $repo rebase -s "theirs" FETCH_HEAD
+        git -C $repo fetch && git -C $repo rebase -s "recursive" -X "theirs" FETCH_HEAD
         [ "$?" -eq 0 ] && echo -e "Scripts succesfully updated\n" || echo -e "Failed to update scripts repo\n"
     else
         echo -e "Failed to update scripts. No git repository found.\n"
