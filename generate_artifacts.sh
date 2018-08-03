70d737eb3523a58565477c7234ba303a0da9288b
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-03 15:50:17 -0400

    generate_artifacts: Tabs to spaces
    Change-Id: I8288954d5a07606789552424d4f1e7fe52a47f7d

--- generate_artifacts.sh
+++ generate_artifacts.sh
@@ -9,7 +9,7 @@ source_common
 update_repo
 
 function get_html_home() {
-# first arg - device name
+    # first arg - device name
     local chipset=`find_chipset $1`
 
     echo /var/www/download.${chipset}.com/public_html
@@ -23,31 +23,31 @@ function sanitize_html_home() {
 
 function fix_html_home_perms() {
     for i in `find /var/www/ -name 'download.*.com'`; do
-	chmod og+r ${i} -R
+        chmod og+r ${i} -R
     done
 }
 
 function link_artifacts() {
-# arg1: find_dir  arg2: out_dir arg3: device
+    # arg1: find_dir  arg2: out_dir arg3: device
     local find_dir=$1
     local out_dir=$2
     local device=$3
     for j in `find $find_dir -mindepth 1 -maxdepth 1 -type f| egrep -iv torrent\|\.part`; do
-	    # ! [ -f $DEST/$(basename $j) ] && 
-	    #echo "Linking $j --> $DEST/$(basename $j) ..."
-	    I_DIR=$(dirname $j)
+        # ! [ -f $DEST/$(basename $j) ] && 
+        #echo "Linking $j --> $DEST/$(basename $j) ..."
+        I_DIR=$(dirname $j)
 
-	    [ -e $out_dir/$(basename $j) ] || ln $j $out_dir/$(basename $j)
-	    if [ $? -ne 0 ] && ! [ -e $out_dir/$(basename $j) ]; then
-		   ln -s $j $out_dir/$(basename $j)
-            fi
+        [ -e $out_dir/$(basename $j) ] || ln $j $out_dir/$(basename $j)
+        if [ $? -ne 0 ] && ! [ -e $out_dir/$(basename $j) ]; then
+            ln -s $j $out_dir/$(basename $j)
+        fi
 
-	    if [ -e "$I_DIR/$device" ]; then
-		    for k in `find "$I_DIR/$device" -type f | egrep -iv torrent\|\.part`; do
-			    [ -e "$out_dir/$(basename $k)" ] && ln $k $out_dir
-			    [ $? -ne 0 ] && ln -s $k $out_dir
-	            done
-            fi
+        if [ -e "$I_DIR/$device" ]; then
+            for k in `find "$I_DIR/$device" -type f | egrep -iv torrent\|\.part`; do
+                [ -e "$out_dir/$(basename $k)" ] && ln $k $out_dir
+                [ $? -ne 0 ] && ln -s $k $out_dir
+            done
+        fi
     done
 }
 
@@ -75,7 +75,7 @@ function generate_artifacts_from_torrent() {
     [ -z "$date_offset" ] && date_offset='3'
 
     for i in `find ${T_OUT} -type f -name ${find_regexp}`; do
-    #    FILE_NAME=$(basename $i | sed s'/.zip//'g);
+        #FILE_NAME=$(basename $i | sed s'/.zip//'g);
         file_name=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
         device_name=$(echo $file_name | cut -d '-' -f ${device_offset})
         build_date=$(echo $file_name | cut -d '_' -f ${date_offset})
@@ -91,9 +91,9 @@ function generate_artifacts_from_torrent() {
         link_artifacts $f_dir $out_dir $device_name
 
         if ! [ -e $dest_torrent ] && [ -f $torrent ]; then
-        ln $torrent $dest_torrent;
+            ln $torrent $dest_torrent;
         fi
-    #    echo
+        #echo
     done
 }
 
@@ -136,7 +136,7 @@ function generate_zram_zip() {
 
     for i in `find /var/www/ -name 'download.*.com'`; do
         echo "Copying zip ${zram_zip} to ${i} ..."
-	mkdir -p $i/public_html/ZRAM
+        mkdir -p $i/public_html/ZRAM
         cp ${zram_zip} $i/public_html/ZRAM
     done
 
@@ -144,8 +144,8 @@ function generate_zram_zip() {
 }
 
 for i in `find ${T_OUT} -type f -name 'TWRP*'`; do
-#    FILE_NAME=$(basename $i | sed s'/.zip//'g);
-#    FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
+    #FILE_NAME=$(basename $i | sed s'/.zip//'g);
+    #FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
     FILE_NAME=$(basename $i | sed s'/\.tar\.[a-z0-9]*\.torrent//'g);
     DEVICE=$(echo $FILE_NAME | cut -d '_' -f 4)
     VERSION=$(echo $FILE_NAME | cut -d '-' -f 2)
@@ -158,21 +158,21 @@ for i in `find ${T_OUT} -type f -name 'TWRP*'`; do
     mkdir -p $OUT_DIR
 
     if ! [ -e $OUT_DIR/${FILE_NAME}.tar ] && [ -f $TAR ]; then
-	ln $TAR $OUT_DIR/${FILE_NAME}.tar
-	#chmod og+r $OUT_DIR/${FILE_NAME}.tar
+        ln $TAR $OUT_DIR/${FILE_NAME}.tar
+        #chmod og+r $OUT_DIR/${FILE_NAME}.tar
     fi
 
     if ! [ -e $DEST_TORRENT ] && [ -f $TORRENT ]; then
         ln $TORRENT $DEST_TORRENT;
-	#chmod og+r $DEST_TORRENT
+        #chmod og+r $DEST_TORRENT
     fi
-#    echo
+    #echo
 done
 
 # TODO: Generate for all chipsets
 for i in `find ${T_OUT} -type f -name 'open_gapps*'`; do
-#    FILE_NAME=$(basename $i | sed s'/.zip//'g);
-#    FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
+    #FILE_NAME=$(basename $i | sed s'/.zip//'g);
+    #FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
     FILE_NAME=$(basename $i | sed s'/\.zip\.[a-z0-9]*\.torrent//'g);
     VERSION=$(echo $FILE_NAME | cut -d '-' -f 3)
     DATE=$(echo $FILE_NAME | cut -d '-' -f 5)
@@ -181,33 +181,33 @@ for i in `find ${T_OUT} -type f -name 'open_gapps*'`; do
     ZIP=$TRANSMISSION_OUT/${FILE_NAME}.zip
     DEST_TORRENT="$OUT_DIR/${FILE_NAME}.torrent"
 
-#    TORRENT=`find $T_OUT/ -name "$FILE_NAME"'*torrent'`
+    #TORRENT=`find $T_OUT/ -name "$FILE_NAME"'*torrent'`
 
     mkdir -p $OUT_DIR
 
     if ! [ -e $OUT_DIR/${FILE_NAME}.zip ] && [ -f $ZIP ]; then
-	ln $ZIP $OUT_DIR/${FILE_NAME}.zip
-	#chmod og+r $OUT_DIR/${FILE_NAME}.zip
+        ln $ZIP $OUT_DIR/${FILE_NAME}.zip
+        #chmod og+r $OUT_DIR/${FILE_NAME}.zip
     fi
 
     if ! [ -e $DEST_TORRENT ] && [ -f $TORRENT ]; then
         ln $TORRENT $DEST_TORRENT;
-	#chmod og+r $DEST_TORRENT
+        #chmod og+r $DEST_TORRENT
     fi
-#    echo
+    #echo
 done
 
 for i in `find ${T_OUT} -type f -name 'rr*torrent'`; do
-#    FILE_NAME=$(basename $i | sed s'/.zip//'g);
+    #FILE_NAME=$(basename $i | sed s'/.zip//'g);
     FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
     DEVICE_NAME=$(echo $FILE_NAME | cut -d '-' -f 3)
     BUILD_DATE=$(echo $FILE_NAME | cut -d '_' -f 3)
     OUT_DIR=`get_html_home $DEVICE_NAME`/ResurrectionRemix/$DEVICE_NAME/$BUILD_DATE
     mkdir -p $OUT_DIR
-#    F_DIR="$(dirname $i)"
+    #F_DIR="$(dirname $i)"
     F_DIR=${TRANSMISSION_OUT}/${FILE_NAME}
 
-#    TORRENT=`find $T_OUT/ -name "$FILE_NAME"'*torrent'`
+    #TORRENT=`find $T_OUT/ -name "$FILE_NAME"'*torrent'`
     TORRENT=$i
     DEST_TORRENT="$OUT_DIR/${FILE_NAME}.torrent"
 
@@ -215,9 +215,9 @@ for i in `find ${T_OUT} -type f -name 'rr*torrent'`; do
 
     if ! [ -e $DEST_TORRENT ] && [ -f $TORRENT ]; then
         ln $TORRENT $DEST_TORRENT;
-#	chmod og+r $DEST_TORRENT
+        #chmod og+r $DEST_TORRENT
     fi
-#    echo
+    #echo
 done
 
 generate_artifacts_from_torrent 'rr*torrent' "ResurrectionRemix" 3 3
