63c2dcb50e795a0636ae8e321f9b5eb46a738e55
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-02 22:39:25 -0400

    generate_artifacts: Add function for ota artifact generation
     - Deduplicate code using function instead of previous logic
    
    Change-Id: I673aad271a379e559d230210773c72e612a28002

--- generate_artifacts-google-1.sh
+++ generate_artifacts-google-1.sh
@@ -62,6 +62,41 @@ fi
 
 sanitize_html_home
 
+function generate_artifacts_from_torrent() {
+    # arg1: find regexp; arg2: dist long (dir) name
+    # arg3: device name offset; arg4: build date offset
+    local find_regexp=$1
+    local dist_name=$2
+    local device_offset=$3
+    local date_offset=$4
+
+    # set some reasonable defaults
+    [ -z "$device_offset" ] && device_offset='3'
+    [ -z "$date_offset" ] && date_offset='3'
+
+    for i in `find ${T_OUT} -type f -name ${find_regexp}`; do
+    #    FILE_NAME=$(basename $i | sed s'/.zip//'g);
+        file_name=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
+        device_name=$(echo $file_name | cut -d '-' -f ${device_offset})
+        build_date=$(echo $file_name | cut -d '_' -f ${date_offset})
+        out_dir=`get_html_home $device_name`/${dist_name}/$device_name/$build_date
+        mkdir -p $out_dir
+        #F_DIR="$(dirname $i)"
+        f_dir=${TRANSMISSION_OUT}/${file_name}
+
+        #TORRENT=`find $T_OUT/ -name "$FILE_NAME"'*torrent'`
+        torrent=$i
+        dest_torrent="$out_dir/${file_name}.torrent"
+
+        link_artifacts $f_dir $out_dir $device_name
+
+        if ! [ -e $dest_torrent ] && [ -f $torrent ]; then
+        ln $torrent $dest_torrent;
+        fi
+    #    echo
+    done
+}
+
 for i in `find ${T_OUT} -type f -name 'TWRP*'`; do
 #    FILE_NAME=$(basename $i | sed s'/.zip//'g);
 #    FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
@@ -139,50 +174,8 @@ for i in `find ${T_OUT} -type f -name 'rr*torrent'`; do
 #    echo
 done
 
-for i in `find ${T_OUT} -type f -name 'lineage-1*torrent'`; do
-#    FILE_NAME=$(basename $i | sed s'/.zip//'g);
-    FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
-    DEVICE_NAME=$(echo $FILE_NAME | cut -d '-' -f 3)
-    BUILD_DATE=$(echo $FILE_NAME | cut -d '_' -f 3)
-    OUT_DIR=`get_html_home $DEVICE_NAME`/LineageOS/$DEVICE_NAME/$BUILD_DATE
-    mkdir -p $OUT_DIR
-#    F_DIR="$(dirname $i)"
-    F_DIR=${TRANSMISSION_OUT}/${FILE_NAME}
-
-#    TORRENT=`find $T_OUT/ -name "$FILE_NAME"'*torrent'`
-    TORRENT=$i
-    DEST_TORRENT="$OUT_DIR/${FILE_NAME}.torrent"
-    
-    link_artifacts $F_DIR $OUT_DIR $DEVICE_NAME
-
-    if ! [ -e $DEST_TORRENT ] && [ -f $TORRENT ]; then
-        ln $TORRENT $DEST_TORRENT;
-#	chmod og+r $DEST_TORRENT
-    fi
-#    echo
-done
-
-for i in `find ${T_OUT} -type f -name 'lineage-go-1*torrent'`; do
-#    FILE_NAME=$(basename $i | sed s'/.zip//'g);
-    FILE_NAME=$(basename $i | sed s'/\.[a-z0-9]*\.torrent//'g);
-    DEVICE_NAME=$(echo $FILE_NAME | cut -d '-' -f 4)
-    BUILD_DATE=$(echo $FILE_NAME | cut -d '_' -f 3)
-    OUT_DIR=`get_html_home $DEVICE_NAME`/LineageOS_Go/$DEVICE_NAME/$BUILD_DATE
-    mkdir -p $OUT_DIR
-#    F_DIR="$(dirname $i)"
-    F_DIR=${TRANSMISSION_OUT}/${FILE_NAME}
-
-#    TORRENT=`find $T_OUT/ -name "$FILE_NAME"'*torrent'`
-    TORRENT=$i
-    DEST_TORRENT="$OUT_DIR/${FILE_NAME}.torrent"
-    
-    link_artifacts $F_DIR $OUT_DIR $DEVICE_NAME
-
-    if ! [ -e $DEST_TORRENT ] && [ -f $TORRENT ]; then
-        ln $TORRENT $DEST_TORRENT;
-#	chmod og+r $DEST_TORRENT
-    fi
-#    echo
-done
+generate_artifacts_from_torrent 'rr*torrent' "ResurrectionRemix" 3 3
+generate_artifacts_from_torrent 'lineage-1*torrent' "LineageOS" 3 3
+generate_artifacts_from_torrent 'lineage-go-1*torrent' "LineageOS_Go" 4 3
 
 fix_html_home_perms
