4fa99ece704c31b1642fa21e889d64e0c8a7a670
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-10 19:04:01 -0400

    build: Add build variables to separate script
     * Move from storing on Jenkins
    
    Change-Id: If4898ffdae316aa50f1fbd8bc3f34cb97f792b87

---
+++ build_script/build_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# telegram
+BUILD_TELEGRAM_CHATID="@Samsung_MSM89XX"
+BUILD_TELEGRAM_TOKEN=""
+
+# ccache
+export CCACHE_COMPRESS="1"
+[ -z "$CCACHE_DIR" ] && export CCACHE_DIR="${JENKINS_HOME}/ccache"
+[ -z "$CCACHE_MAXSIZE" ] && export CCACHE_MAXSIZE="60G"
+export USE_CCACHE="1"
+
+[ -z "$MAX_JOB_NUMBER" ] && MAX_JOB_NUMBER="15"
