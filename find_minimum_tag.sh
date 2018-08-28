a59edb5c8a979bf451d36420f3e7c1891bbef645
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-27 23:53:03 -0400

    find_minimum_tag: Rewrite script
     - Add usage/help text
    
     - Read important variables from arguments passed
       instead of hardcoding them
    
    Change-Id: I7897b3d775b1d179dd7e4a3321d41dea7b71f349

--- find_minimum_tag.sh
+++ find_minimum_tag.sh
@@ -1,27 +1,75 @@
 #!/bin/bash
 
-tags_file="tags.txt"
-remotes_file="remotes.txt"
-out_file="report.txt"
+function print_help() {
+    echo "Usage: `basename $0` [OPTIONS] "
+    echo "  -b | --branches   Path to file with list of branches"
+    echo "  -d | --path       Path to git repo directory"
+    echo "  -o | --out        Path to output file (for report)"
+    echo "                    if none is specified, print to stdout"
+    echo "  -r | --remote     Remote to use for fetching tags/branches"
+    echo "  -t | --tags       Path to file with list of tags"
+    echo "  -h | --help       Print this message"
+    exit 0
+}
 
-echo "Merge report" >> ${out_file}
-echo -e "============\n" >> ${out_file}
+if [ "x$1" == "x" ]; then
+    print_help
+fi
 
-
-for tag in `cat ${tags_file}`; do
-	git fetch caf ${tag}
-	git pull --no-commit caf ${tag}
-	conflict_count=`git status | grep 'both modified' | wc -l`
-	git merge --abort
-	echo -e "\tTag ${tag} has ${conflict_count} conflicts." >> ${out_file}
+while [ "$1" != "" ]; do
+    case $1 in
+        -b | --branches)        shift
+                                BRANCH_FILE=$1
+                                ;;
+        -d | --path )           shift
+                                REPO_DIR=$1
+                                ;;
+        -o | --out)             shift
+                                REPORT_OUT_FILE=$1
+                                ;;
+        -r | --remote)          shift
+                                REMOTE=$1
+                                ;;
+        -t | --tags)            shift
+                                TAGS_FILE=$1
+                                ;;
+        *)                      print_help
+                                ;;
+    esac
+    shift
 done
 
-echo -e "\n==================\n" >> ${out_file}
+if [ "x${BRANCH_FILE}" == "x" ] && [ "x${TAGS_FILE}" == "x" ]; then
+    echo "Error: No branch or tag file specified!" >> /dev/stderr
+fi
 
-for remote in `cat ${remotes_file}`; do
-	git fetch caf ${remote}
-	git pull --no-commit caf ${remote}
-	conflict_count=`git status | grep 'both modified' | wc -l`
-	git merge --abort
-	echo -e "\tBranch ${remote} has ${conflict_count} conflicts." >> ${out_file}
+if [ "x${REPO_DIR}" == "x" ]; then
+    REPO_DIR=$PWD
+fi
+
+if [ "x${REPORT_OUT_FILE}" == "x" ]; then
+    REPORT_OUT_FILE=/dev/stdout
+fi
+
+echo "Merge report" >> ${REPORT_OUT_FILE}
+echo -e "============\n" >> ${REPORT_OUT_FILE}
+
+for tag in `cat ${TAGS_FILE}`; do
+	git -C ${REPO_DIR} fetch ${REMOTE} ${tag}
+	git -C ${REPO_DIR} clean -df
+	git -C ${REPO_DIR} pull  --no-commit ${REMOTE} ${tag} --allow-unrelated-histories
+	conflict_count=`git -C ${REPO_DIR} status | grep 'both modified' | wc -l`
+	git -C ${REPO_DIR} merge --abort
+	echo -e "\tTag ${tag} has ${conflict_count} conflicts." >> ${REPORT_OUT_FILE}
+done
+
+echo -e "\n==================\n" >> ${REPORT_OUT_FILE}
+
+for branch in `cat ${BRANCH_FILE}`; do
+	git -C ${REPO_DIR} fetch ${REMOTE} ${branch}
+	git -C ${REPO_DIR} clean -df
+	git -C ${REPO_DIR} pull  --no-commit ${REMOTE} ${tag} --allow-unrelated-histories
+	conflict_count=`git -C ${REPO_DIR} status | grep 'both modified' | wc -l`
+	git -C ${REPO_DIR} merge --abort
+	echo -e "\tBranch ${branch} has ${conflict_count} conflicts." >> ${REPORT_OUT_FILE}
 done
