c4153d97b693790fed38e431be0f9436516e597b
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-29 00:32:13 -0400

    Add script for generating fake source dir structure
     - Only really useful for regenerating vendor without messing with an actual source dir
    
    Change-Id: Ib4ab86920f267f5aeeb9a1f7da9235e7e865932d

---
+++ generate_fake_source_structure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# TODO: add usage, allow specifying repo dir and target fake source dir
+
+target_dir=lineage
+
+for i in `ls|grep android_`; do
+    dir=`echo $i|sed s/android_//g|sed s/\.git//g | sed s'/_/\//'g`;
+    mkdir -p $target_dir/$dir; ln -s $PWD/$i/.git $target_dir/$dir/;
+    git -C $target_dir/$dir reset --hard;
+done
+
+for i in `ls|grep proprietary_`; do
+    dir=`echo $i|sed s/proprietary_//g|sed s/\.git//g | sed s'/_/\//'g`;
+    mkdir -p $target_dir/$dir; ln -s $PWD/$i/.git $target_dir/$dir/;
+    git -C $target_dir/$dir reset --hard;
+done
+
+git clone https://github.com/LineageOS/android_vendor_lineage.git $target_dir/vendor/lineage --depth=1
