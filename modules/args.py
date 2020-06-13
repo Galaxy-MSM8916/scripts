702c1e79da12b77719514b5ade064ae995638eae
Vincent Zvikaramba <zvikovincent@gmail.com>
2020-06-12 22:50:09 -0400

    Rework arguments and return parsed args
    Change-Id: I76ad3953b6242051c81cee70b6b423133722ce3f

--- modules/args.py
+++ modules/args.py
@@ -11,20 +11,20 @@ def parse_args():
 
     parser.add_argument('-d', '--device', metavar='device', type=str, nargs=1, help='Device codename.', choices=helpers.get_devices())
 
-    parser.add_argument('--distro', '--distribution', metavar='distribution', type=str, nargs=1, help='Distribution to build.', choices=helpers.get_distros())
+    parser.add_argument( '--distribution', '--distro', metavar='distribution', type=str, nargs=1, help='Distribution to build.', choices=helpers.get_distros())
 
     parser.add_argument('--type', metavar='type', type=str, nargs=1, help='Type of build.', choices=helpers.get_build_types())
 
-    parser.add_argument('--pick', metavar='pick', type=int, nargs='+', help='Pick msm8916 gerrit changes.')
-    parser.add_argument('--pick-lineage', metavar='pick-lineage', type=int, nargs='+', help='Pick lineage gerrit changes.')
+    parser.add_argument('--pick', metavar='number', type=int, nargs='+', help='Pick msm8916 gerrit changes.')
+    parser.add_argument('--pick-lineage', metavar='number', type=int, nargs='+', help='Pick lineage gerrit changes.')
 
-    parser.add_argument('--pick-topic', metavar='pick-topic', type=str, nargs='+', help='Pick msm8916 gerrit topics.')
-    parser.add_argument('--pick-lineage-topic', metavar='pick-lineage-topic', type=str, nargs='+', help='Pick lineage gerrit topics.')
+    parser.add_argument('--pick-topic', metavar='topic', type=str, nargs='+', help='Pick msm8916 gerrit topics.')
+    parser.add_argument('--pick-lineage-topic', metavar='topic', type=str, nargs='+', help='Pick lineage gerrit topics.')
 
-    parser.add_argument('-s', '--silent', metavar='silent', help='Silence telegram notifications.')
+    parser.add_argument('-s', '--silent', action="store_true", help='Silence telegram notifications.')
 
-    parser.add_argument('--no-clean', metavar='no-clean', help="Don't clean build top after completion.")
+    parser.add_argument('--clean', action="store_true", help="clean build top after completion.")
 
-    parser.add_argument('--days', metavar='days', type=int, nargs=1, help='Number of days of changelogs to generate.')
+    parser.add_argument('--days', metavar='num', type=int, nargs=1, help='Number of days of changelogs to generate.')
 
-    args = parser.parse_args()
+    return parser.parse_args().__dict__
