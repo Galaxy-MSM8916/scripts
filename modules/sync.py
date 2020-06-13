319259568b7fcb8c2c51eb10b09887e26f228518
Vincent Zvikaramba <zvikovincent@gmail.com>
2020-06-13 03:19:02 -0400

    sync: Add method to initialise repo source dir
    Change-Id: I24e0560f1f383f026d37a6e6af581bcc0642fda8

--- modules/sync.py
+++ modules/sync.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-import requests 
+import os
+import subprocess
+import requests
+from . import distros
 
 manifest_repo_url = "https://raw.githubusercontent.com/Galaxy-MSM8916/local_manifests/master"
 
@@ -17,5 +20,57 @@ def get_manifest(distro, version):
 
     return r.text
 
+def initialise_dist_repo(build_dir, distro, version):
+    """
+    Initialise repo source directory for distro with version
+    """
 
+    top_dir = os.environ['PWD']
+
+    repo_path = top_dir + '/tools/repo'
+    if not os.path.exists(repo_path):
+        print("Error: could not find repo tool")
+        os._exit(1)
+
+    repo_url = distros.get_distro_repo_url(distro)
+    if repo_url == None:
+        print("Error: could not determine repo url")
+        os._exit(1)
+
+    repo_dir = build_dir + "/" + distro + "-" + version
+
+    os.makedirs(repo_dir, exist_ok=True)
+    os.chdir(repo_dir)
+
+    if os.getcwd == top_dir:
+        print("Error: failed to change directory")
+        os._exit(1)
+
+    versions = []
+    version_prefix = distros._get_distro_dict_value(distro, "init_prefix")
+    if version_prefix == None or len(version_prefix) == 0:
+        versions.append(version)
+    else:
+        versions = [ v + version for v in version_prefix]
+
+    initialised = False
+    for v in versions:
+
+        args = [repo_path, "init", "-u", repo_url, "-b", v]
+
+        try:
+            result = subprocess.run(args, timeout=10, input="", text=True)
+
+            if result.returncode == 0:
+                initialised = True
+                break
+        except subprocess.TimeoutExpired:
+            print("Timed out initialising repo")
+            continue
+
+    if not initialised:
+        print("Error: could not initialise repo")
+        os._exit(1)
+
+    os.chdir(top_dir)
     
