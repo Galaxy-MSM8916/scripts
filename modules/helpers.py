fe76e4fde2d9a1eb3e0353b9df71230b5b33b569
Vincent Zvikaramba <zvikovincent@gmail.com>
2020-06-12 22:10:41 -0400

    modules: helpers: Add missing helpers (and some)
    Change-Id: I15b58d7a282f5d3fad1a665e2c5bb79651ebf6e2

--- modules/helpers.py
+++ modules/helpers.py
@@ -5,21 +5,72 @@ except ImportError:
     import conf_template as conf
 
 def get_targets():
-    try:
-        targets = conf.targets
-    except:
-        targets = []
+    """
+    Return all valid targets
+    """
+
+    return conf.targets
+
+def get_devices():
+    """
+    Return all valid devices
+    """
+
+    devices = set()
+
+    for variant_map in conf.devices.values():
+        devices.update(set(variant_map.keys()))
+
+    return devices
+
+def get_distros():
+    """
+    Return all valid distros
+    """
+
+    distros = set()
+
+    for key in conf.distros:
+        distros.add(key)
+
+        if "variants" in conf.distros[key]:
+            distros.update(set(conf.distros[key]["variants"]))
+
+    return distros
+
+def get_build_types():
+    """
+    Return all valid build types
+    """
+
+    types = set()
+
+    for key in conf.distros:
+        if "types" in conf.distros[key]:
+            types.update(set(conf.distros[key]["types"]))
+
+def get_job_targets(job):
+    """
+    Return targets for job
+    """
+
+    targets = set()
+
+    if job in conf.jobs:
+        if "targets" in conf.jobs[job]:
+            targets.update(set(conf.jobs[job]["targets"]))
 
     return targets
 
-def get_devices():
+def get_job_build_types(job):
+    """
+    Return build types for job
+    """
 
-    devices = []
+    types = set()
 
-    try:
-        for i in conf.devices.values():
-            devices.extend(i)
-    except:
-        pass
+    if job in conf.jobs:
+        if "types" in conf.distros[job]:
+            types.update(set(conf.jobs[job]["types"]))
 
-    return devices
+    return types
