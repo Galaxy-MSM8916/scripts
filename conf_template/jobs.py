defb08dd1bd6747feb809de04e0318b8d0d2ec60
Vincent Zvikaramba <zvikovincent@gmail.com>
2020-06-16 17:20:52 -0400

    conf: jobs: Add ennvars for github-release tool
    Change-Id: I8197e5469156b1ef144538379a2df0b1e472e00c

--- conf_template/jobs.py
+++ conf_template/jobs.py
@@ -4,6 +4,12 @@
 # map of information pertinent to running automated builds
 # map keys are distro short names (see distributions.py)
 
+envvars = {
+    "GITHUB_TOKEN": "",
+    "GITHUB_USER": "Galaxy-MSM8916",
+    "GITHUB_REPO": "releases",
+}
+
 jobs = {
     "lineage": {
         "17.1": {
