317f16a7447e706425a86ab5843a24d7c0678d2e
Vincent Zvikaramba <zvikovincent@gmail.com>
2020-06-16 02:44:06 -0400

    modules: jobs: Update and add new methods for jobs config queries
    Change-Id: I3bac977e813ed7364a76c25bf47ef289f6e56fa4

--- modules/jobs.py
+++ modules/jobs.py
@@ -2,28 +2,106 @@
 
 from . import config
 
-def get_targets(distro):
+def get_jobs_dict_value(distro, version, key):
+    """
+    Return value mapped to key in distro dictionary
+    """
+    if distro in config.jobs:
+        if version in config.jobs[distro]:
+            if key in config.jobs[distro][version]:
+                return config.jobs[distro][version][key]
+
+    return None
+
+def get_targets(distro, version):
     """
     Return targets for distro
     """
 
-    targets = set()
+    key = "targets"
 
-    if distro in config.jobs:
-        if "targets" in config.jobs[distro]:
-            targets.update(set(config.jobs[distro]["targets"]))
+    res = get_jobs_dict_value(distro, version, key)
+    if res == None:
+        return []
 
-    return targets
+    return res
 
-def get_build_variants(distro):
+def get_build_variants(distro, version):
     """
     Return build variants for distro
     """
 
-    build_variants = set()
+    key = "build_variants"
+
+    res = get_jobs_dict_value(distro, version, key)
+    if res == None:
+        return []
+
+    return res
+
+def get_distro_versions(distro):
+    """
+    Return versions mapped to distro
+    """
 
     if distro in config.jobs:
-        if "build_variants" in config.distros[distro]:
-            build_variants.update(set(config.jobs[distro]["build_variants"]))
+        return config.jobs[distro].keys()
 
-    return build_variants
+    return set()
+
+def get_devices(distro, version):
+    """
+    Return devices for distro
+    """
+
+    key = "devices"
+
+    res = get_jobs_dict_value(distro, version, key)
+    if res == None:
+        return []
+
+    return res
+
+def get_repopicks(distro, version):
+    """
+    Return pair of repopick change ids and topics for distro
+    """
+
+    key = "picks"
+
+    res = get_jobs_dict_value(distro, version, key)
+    if res == None:
+        return set(), set()
+
+    picks = set()
+    topics = set()
+
+    for pick in res:
+        try:
+            picks.add(int(pick))
+        except ValueError:
+            topics.add(pick)
+
+    return picks, topics
+
+def get_lineage_repopicks(distro, version):
+    """
+    Return pair of lineage repopick change ids and topics for distro
+    """
+
+    key = "picks-lineage"
+
+    res = get_jobs_dict_value(distro, version, key)
+    if res == None:
+        return set(), set()
+
+    picks = set()
+    topics = set()
+
+    for pick in res:
+        try:
+            picks.add(int(pick))
+        except ValueError:
+            topics.add(pick)
+
+    return picks, topics
