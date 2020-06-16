d227b6d257381b2a2f49ffe719b0bdc0e9b86d83
Vincent Zvikaramba <zvikovincent@gmail.com>
2020-06-15 20:42:37 -0400

    modules: build: Add methods to get artifact paths
    Change-Id: Iad9f8dce83498e2f10adcd5400d11e001868a077

--- modules/build.py
+++ modules/build.py
@@ -45,5 +45,52 @@ def build_target(build_dir, distro, version, device, target, build_variant):
     if res.returncode != 0:
         print("Build failed with return code " + str(res.returncode))
         os._exit(res.returncode)
-    
+
     os.chdir(top_dir)
+
+def get_bootimage_path(build_dir, distro, version, device):
+    """
+    Return path to boot image, or None if not found
+    """
+    repo_dir = distros.get_distro_repo_dir(build_dir, distro, version)
+
+    bootimage_path = repo_dir + "/out/target/product/" \
+         + device + "/boot.img"
+
+    if os.path.exists(bootimage_path):
+        return bootimage_path
+
+    return None
+
+def get_recoveryimage_path(build_dir, distro, version, device):
+    """
+    Return path to recovery image, or None if not found
+    """
+    repo_dir = distros.get_distro_repo_dir(build_dir, distro, version)
+
+    recoveryimage_path = repo_dir + "/out/target/product/" \
+         + device + "/recovery.img"
+
+    if os.path.exists(recoveryimage_path):
+        return recoveryimage_path
+
+    return None
+
+def get_otapackage_path(build_dir, distro, version, device):
+    """
+    Return path to ota package, or None if not found
+    """
+    repo_dir = distros.get_distro_repo_dir(build_dir, distro, version)
+    out_path = repo_dir + "/out/target/product/" + device
+
+    if not os.path.exists(out_path):
+        return None
+
+    dir_contents = os.listdir(out_path)
+
+    for file in dir_contents:
+        if file.endswith(".zip"):
+            if device in file:
+                return out_path + "/" + file
+
+    return None
