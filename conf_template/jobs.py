c432fff08a3503148ae6f193a6a6362da59b94bb
Vincent Zvikaramba <zvikovincent@gmail.com>
2020-06-18 19:39:13 -0400

    Upload to sourceforge
    Change-Id: Ib7f0e8617754d9145f41a673caa4c4e39df657f2

--- conf_template/jobs.py
+++ conf_template/jobs.py
@@ -8,6 +8,10 @@ envvars = {
     #"GITHUB_TOKEN": "",
     "GITHUB_USER": "Galaxy-MSM8916",
     "GITHUB_REPO": "releases",
+    "SF_USER": "galaxy-msm8916",
+    "SF_PROJECT": "galaxy-msm8916",
+    "SF_HOST": "frs.sourceforge.net",
+    #"SF_IDENTITY_KEY": "",
 }
 
 jobs = {
